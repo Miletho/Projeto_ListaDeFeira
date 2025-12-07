93fa9b7dad141a14e44af491abaec21caedf1fc5
Thales Soares <205557856+Miletho@users.noreply.github.com>
2025-12-07 13:39:06 -0300

    Refactor Hortalica class to inherit from ItemBase

--- Hortalica.cpp
+++ Hortalica.cpp
@@ -1,11 +1,12 @@
 #include "Hortalica.h"
 #include <iostream>
 
-Hortalica::Hortalica(const std::string& n, int q, bool o) : Feira(n, q), organica(o) {}
+Hortalica::Hortalica(const std::string& n, int q, bool o) : ItemBase(n, q), organica(o) {}
 
 void Hortalica::exibirInfo() const {
     std::cout << "[Hortaliça] " << nome
               << " | Quantidade: " << quantidade
               << " | Orgânica: " << (organica ? "Sim" : "Não") << std::endl;
 }
+
 
