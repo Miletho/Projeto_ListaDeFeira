eeeb5f4640e41a88ea5dbf2bc4c61d96d5e9037e
Thales Soares <205557856+Miletho@users.noreply.github.com>
2025-12-07 13:39:21 -0300

    Refactor ProdutoLimpeza to inherit from ItemBase

--- ProdutoLimpeza.cpp
+++ ProdutoLimpeza.cpp
@@ -1,11 +1,12 @@
 #include "ProdutoLimpeza.h"
 #include <iostream>
 
-ProdutoLimpeza::ProdutoLimpeza(const std::string& n, int q, const std::string& u) : Feira(n, q), uso(u) {}
+ProdutoLimpeza::ProdutoLimpeza(const std::string& n, int q, const std::string& u) : ItemBase(n, q), uso(u) {}
 
 void ProdutoLimpeza::exibirInfo() const {
     std::cout << "[Produto de Limpeza] " << nome
               << " | Quantidade: " << quantidade
               << " | Uso: " << uso << std::endl;
 }
+
 
