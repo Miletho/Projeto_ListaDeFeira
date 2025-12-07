9fd535ccc5ae240ad46e6ed864a67b94475963c0
Thales Soares <205557856+Miletho@users.noreply.github.com>
2025-12-07 13:37:16 -0300

    Update AlimentoBase to inherit from ItemBase

--- AlimentoBase.cpp
+++ AlimentoBase.cpp
@@ -1,10 +1,11 @@
 #include "AlimentoBase.h"
 #include <iostream>
 
-AlimentoBase::AlimentoBase(const std::string& n, int q) : Feira(n, q) {}
+AlimentoBase::AlimentoBase(const std::string& n, int q) : ItemBase(n, q) {}
 
 void AlimentoBase::exibirInfo() const {
     std::cout << "[Alimento Base] " << nome
               << " | Quantidade: " << quantidade << std::endl;
 }
+
 
