4b94811f7ca3565c46b95594ee299d4160f4950a
Thales Soares <205557856+Miletho@users.noreply.github.com>
2025-12-07 13:33:57 -0300

    Refactor Acougue class to inherit from ItemBase

--- Acougue.cpp
+++ Acougue.cpp
@@ -1,11 +1,12 @@
 #include "Acougue.h"
 #include <iostream>
 
-Acougue::Acougue(const std::string& n, int q, const std::string& c) : Feira(n, q), corte(c) {}
+Acougue::Acougue(const std::string& n, int q, const std::string& c) : ItemBase(n, q), corte(c) {}
 
 void Acougue::exibirInfo() const {
     std::cout << "[Açougue] " << nome
               << " | Quantidade: " << quantidade
               << " | Corte: " << corte << std::endl;
 }
+
 
