d5029b2f1f6fb34d2f99a03ce9a4c9de3def7ac4
Thales Soares <205557856+Miletho@users.noreply.github.com>
2025-12-07 13:41:15 -0300

    Delete Feira.cpp

--- Feira.cpp
+++
@@ -1,8 +0,0 @@
-#include "Feira.h"
-
-Feira::Feira(const std::string& n, int q) : nome(n), quantidade(q) {}
-
-std::string Feira::getNome() const { return nome; }
-int Feira::getQuantidade() const { return quantidade; }
-void Feira::setQuantidade(int q) { quantidade = q; }
-
